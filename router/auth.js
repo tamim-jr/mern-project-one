bdaf87739f87e3490e29a7450a845c8f67c222c9
tamim-jr <internationalinf24@gmail.com>
2023-09-03 02:21:28 +0600

    use promises and async for recive user data and send it to database mongdb

--- router/auth.js
+++ router/auth.js
@@ -5,35 +5,70 @@ const router = express.Router();
 require('../db/conn');
 const User = require('../model/userSchema');
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     res.send(`Hello World! from server router js`);
 });
 
-router.post('/register', (req, res)=>{
+//all of this code are for user data send from uaser section and collect to databse
 
-    const {name, email, phone, work, password, cpassword} = req.body;
+//using promises
+// router.post('/register', (req, res)=>{
 
-    if(!name || !email || !phone || !work || !password || !cpassword){
-        return res.status(422).json({err:"Please filled the field properly"});
+//     const {name, email, phone, work, password, cpassword} = req.body;
+
+//     if(!name || !email || !phone || !work || !password || !cpassword){
+//         return res.status(422).json({err:"Please filled the field properly"});
+//     }
+
+//     User.findOne({email:email})
+//     .then((userExist) => {
+//         if(userExist){
+//             return res.status(422).json({err:"This email already Exist"});
+//         }
+
+//         const user = new User({name, email, phone, work, password, cpassword});
+//         user.save().then(() =>{
+//             res.status(201).json({msg: "user registered successfuly"});
+//         }).catch((err) => res.status(500).json({err:"Failed to segistered"}));
+
+//     }).catch(err => {console.log(err);});
+//     // console.log(name);
+//     // console.log(email);
+//     // // res.json({msg: req.body});
+//     // res.send('This is my register page');
+
+// });
+
+
+//using async
+
+router.post('/register', async (req, res) => {
+
+    const { name, email, phone, work, password, cpassword } = req.body;
+
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({ err: "Please filled the field properly" });
     }
-
-    User.findOne({email:email})
-    .then((userExist) => {
-        if(userExist){
-            return res.status(422).json({err:"This email already Exist"});
+    try {
+        const userExist = await User.findOne({ email: email });
+        if (userExist) {
+            return res.status(422).json({ err: "This email already Exist" });
         }
+        const user = new User({ name, email, phone, work, password, cpassword });
 
-        const user = new User({name, email, phone, work, password, cpassword});
-        user.save().then(() =>{
-            res.status(201).json({msg: "user registered successfuly"});
-        }).catch((err) => res.status(500).json({err:"Failed to segistered"}));
-
-    }).catch(err => {console.log(err);});
-    // console.log(name);
-    // console.log(email);
-    // // res.json({msg: req.body});
-    // res.send('This is my register page');
-
+        //some bigger
+        // const userRegister = await user.save();
+        // if(userRegister){
+        //     res.status(201).json({ msg: "user registered successfuly" });
+        // }else{
+        //     res.status(500).json({ err: "Failed to segistered" })
+        // }
+        //more then shorter
+        await user.save();
+        res.status(201).json({ msg: "user registered successfuly" });
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 module.exports = router;
