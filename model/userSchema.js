46be6713324d7844a401bff4b000590d3bc1ee31
tamim-jr <internationalinf24@gmail.com>
2023-09-02 23:49:45 +0600

    make usersSchema

--- model/userSchema.js
+++ model/userSchema.js
@@ -1,5 +1,27 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    
+    name :{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    phone:{
+        type: Number,
+        tequired: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    cpassword:{
+        type: String,
+        required: true
+    }
 })
+
+const User = mongoose.model('USER', userSchema);
+module.exports = User;
